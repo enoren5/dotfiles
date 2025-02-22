f52b4f741475745f6f1e0185a5dd4a8b09a72983
Joseph  Newton <drone4four@gmail.com>
2025-02-22 15:13:27 -0500

    Begin tracking custom NvChad+nvim configs

---
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -0,0 +1,26 @@
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
+
+---
+-- @type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "catppuccin",
+  
+
+  changed_themes = {
+     catppuccin = {
+        
+       base_16 = { base00 ="#303446"},
+        
+       base_30 = { 
+        black = "#303446",
+        darker_black = "#292c39",
+        },
+      },
+  },
+}
+
+return M
